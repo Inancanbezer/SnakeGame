11758cdb6c4cc9244cd21e981b733b95279c8768
Inancanbezer <78171335+Inancanbezer@users.noreply.github.com>
2023-11-12 00:00:05 +0300

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,30 @@
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+
+public class SnakeGame extends JFrame{
+
+    public SnakeGame(String title) throws HeadlessException {
+        super(title);
+    }
+   
+    public static void main(String[] args){
+        
+        SnakeGame ekran = new SnakeGame("Snake Oyunu"); 
+        ekran.setResizable(false);
+        ekran.setFocusable(false);
+        
+        ekran.setSize(1100,800);
+        ekran.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Oyun oyun = new Oyun(); 
+        oyun.requestFocus(); 
+        oyun.addKeyListener(oyun);
+        oyun.setFocusable(true);
+        oyun.setFocusTraversalKeysEnabled(false);
+        ekran.add(oyun);
+        ekran.setVisible(true);
+    }    
+
+}
